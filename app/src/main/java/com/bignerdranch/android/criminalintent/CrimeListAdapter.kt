91805bdfcbbf3736c2e8c67e52877ec76bdab505
hrl-2024 <hrl@bu.edu>
2023-10-13 13:14:53 -0400

    Cleaning Up Binding List Items

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -5,7 +5,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeBinding
 
-class CrimeHolder (val binding: ListItemCrimeBinding) : RecyclerView.ViewHolder(binding.root) {
+class CrimeHolder (private val binding: ListItemCrimeBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(crime: Crime) {
+        binding.crimeTitle.text = crime.title
+        binding.crimeDate.text = crime.date.toString()
+    }
 }
 
 class CrimeListAdapter (private val crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
@@ -18,10 +22,7 @@ class CrimeListAdapter (private val crimes: List<Crime>) : RecyclerView.Adapter<
 
     override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
         val crime = crimes[position]
-        holder.binding.apply {
-            crimeTitle.text = crime.title
-            crimeDate.text = crime.date.toString()
-        }
+        holder.bind(crime)
     }
 
     override fun getItemCount() = crimes.size
