305b0420350e661d41fc1ac051a24c56fd9e8065
hrl-2024 <hrl@bu.edu>
2023-10-17 14:13:56 -0400

    Accessing the Database Using the Repository Pattern

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -0,0 +1,40 @@
+package com.bignerdranch.android.criminalintent
+
+import android.content.Context
+import androidx.room.Room
+import com.bignerdranch.android.criminalintent.database.CrimeDatabase
+import java.util.UUID
+
+private const val DATABASE_NAME = "crime-database"
+
+// This class is a singleton (there will only ever be 1 instance of it in the app process.
+// Data exists when app is in memory, but not when app is closed (onDestroy).
+// Useful to pass data between components
+class CrimeRepository private constructor(context: Context) {
+
+    private val database: CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+        ).build()
+
+    suspend fun getCrime(): List<Crime> = database.crimeDao().getCrimes()
+
+    suspend fun getCrime(id: UUID) : Crime = database.crimeDao().getCrime(id)
+
+    companion object {
+        private var INSTANCE: CrimeRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = CrimeRepository(context)
+            }
+        }
+
+        fun get(): CrimeRepository {
+            return INSTANCE ?:
+                throw IllegalStateException("CrimeRepository must be initialized")
+        }
+    }
+
+}
