6636ccaedeed04e5d02ccab75ba4bf42598191ad
hrl-2024 <hrl@bu.edu>
2023-11-08 10:49:01 -0500

    Performing Navigation: Navigating to the detail screen

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -8,12 +8,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeBinding
 
 class CrimeHolder (private val binding: ListItemCrimeBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(crime: Crime) {
+    fun bind(crime: Crime, onCrimeClicked: () -> Unit) {
         binding.crimeTitle.text = crime.title
         binding.crimeDate.text = crime.date.toString()
 
         binding.root.setOnClickListener {
-            Toast.makeText(binding.root.context, "${crime.title} pressed!", Toast.LENGTH_SHORT).show()
+            onCrimeClicked()
         }
 
         binding.crimeSolved.visibility = if (crime.isSolved) {
@@ -24,7 +24,7 @@ class CrimeHolder (private val binding: ListItemCrimeBinding) : RecyclerView.Vie
     }
 }
 
-class CrimeListAdapter (private val crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
+class CrimeListAdapter (private val crimes: List<Crime>, private val onCrimeClicked: () -> Unit) : RecyclerView.Adapter<CrimeHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -34,7 +34,7 @@ class CrimeListAdapter (private val crimes: List<Crime>) : RecyclerView.Adapter<
 
     override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
         val crime = crimes[position]
-        holder.bind(crime)
+        holder.bind(crime, onCrimeClicked)
     }
 
     override fun getItemCount() = crimes.size
