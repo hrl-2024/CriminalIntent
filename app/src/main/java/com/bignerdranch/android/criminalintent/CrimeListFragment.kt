a11082710a2695cd3c23b4ce8457a865892b8ac7
hrl-2024 <hrl@bu.edu>
2023-11-13 11:39:04 -0500

    Menus: Creating the menu (Note: tho a lot has deprecated already)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -3,6 +3,8 @@ package com.bignerdranch.android.criminalintent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -26,6 +28,11 @@ class CrimeListFragment : Fragment() {
     }
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)   // let system know that CrimeListFragment needs to receive menu callbacks.
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -60,5 +67,10 @@ class CrimeListFragment : Fragment() {
         super.onDestroyView()
         Log.d(TAG, "onDestroyView called")
         _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
     }
 }
